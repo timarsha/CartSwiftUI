929d8f3807a97f93bd46aa38a894d9a325064612
Igor <igortimarsha@gmail.com>
2020-10-12 23:37:10 +0300

    Initial Commit

---
+++ CartSwiftUI/CartSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CartSwiftUIApp.swift
+//  CartSwiftUI
+//
+//  Created by Игорь Тимарша on 12.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct CartSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CartSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CartSwiftUI
+//
+//  Created by Игорь Тимарша on 12.10.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
